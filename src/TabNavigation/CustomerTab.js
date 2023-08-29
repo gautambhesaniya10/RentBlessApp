1c17586ed35eec106433b0ae11f3a9e48082750c
Gautam <gb949308@gmail.com>
2023-08-29 16:57:29 +0530

    like page done

--- src/TabNavigation/CustomerTab.js
+++ src/TabNavigation/CustomerTab.js
@@ -4,9 +4,11 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import HomePage from '../screens/Customer/HomePage';
 import LikeScreen from '../screens/Customer/pages/LikeScreen';
+import {useSelector} from 'react-redux';
 
 const CustomerTab = () => {
   const Tab = createBottomTabNavigator();
+  const {userProfile} = useSelector(state => state?.user);
 
   return (
     <Tab.Navigator
@@ -35,7 +37,16 @@ const CustomerTab = () => {
           headerShown: false,
           tabBarLabel: 'Like',
           tabBarIcon: ({color, size}) => (
-            <Icon name="heart" color={color} size={20} />
+            <View style={styles.mainLikeTabDiv}>
+              <Icon name="heart" color={color} size={22} />
+              {userProfile?.product_like_list?.length > 0 && (
+                <View style={styles.likeCountMain}>
+                  <Text style={styles.likeCount}>
+                    {userProfile?.product_like_list?.length}
+                  </Text>
+                </View>
+              )}
+            </View>
           ),
         }}
       />
@@ -45,4 +56,24 @@ const CustomerTab = () => {
 
 export default CustomerTab;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainLikeTabDiv: {
+    position: 'relative',
+  },
+  likeCountMain: {
+    backgroundColor: 'red',
+    width: 16,
+    height: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    position: 'absolute',
+    left: 15,
+    bottom: 9,
+  },
+  likeCount: {
+    color: 'white',
+    fontSize: 12,
+    fontWeight: '600',
+  },
+});
