ad602d8fabeb96ef6faf3db009f288ca15ea269e
Gautam <gb949308@gmail.com>
2023-10-09 15:06:27 +0530

    env setup

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,9 @@
+import {NEXT_PUBLIC_API_URL} from '@env';
+
 const appConfig = {
   // appUrl: 'http://192.168.1.13:8080/graphql',
-  appUrl: 'https://api.rentbless.com/graphql',
+  // appUrl: 'https://api.rentbless.com/graphql',
+  appUrl: NEXT_PUBLIC_API_URL,
 };
 
 export default appConfig;
