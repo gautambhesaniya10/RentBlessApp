d292d32729c784ec1a0599be813f43736efe1dcd
Gautam <gb949308@gmail.com>
2023-10-26 11:59:48 +0530

    remove unuse

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 import React from 'react';
 import 'react-native-gesture-handler';
 import AppNavigator from './AppNavigator';
-import {PaperProvider} from 'react-native-paper';
 import {NativeBaseProvider, ToastProvider} from 'native-base';
 
 const App = () => {
   return (
     <>
-      {/* <PaperProvider> */}
       <NativeBaseProvider>
         <ToastProvider>
           <AppNavigator />
         </ToastProvider>
       </NativeBaseProvider>
-
-      {/* </PaperProvider> */}
     </>
   );
 };
