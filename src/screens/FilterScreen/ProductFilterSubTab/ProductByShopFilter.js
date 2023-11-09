0090971e124f2c3e085dab9007b3e998609ef6dd
Gautam <gb949308@gmail.com>
2023-11-09 16:17:57 +0530

    search field add on filter

--- src/screens/FilterScreen/ProductFilterSubTab/ProductByShopFilter.js
+++ src/screens/FilterScreen/ProductFilterSubTab/ProductByShopFilter.js
@@ -1,11 +1,13 @@
 import {StyleSheet, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {useSelector} from 'react-redux';
 import {CheckBox} from 'react-native-elements';
 import {capitalizeString} from '../../../common/CapitalizeString';
+import SearchTextFiled from '../../../common/SearchTextFiled';
 
 const ProductByShopFilter = ({selectedShopData, setSelectedShopData}) => {
   const {allShopsLists} = useSelector(state => state?.shops);
+  const [searchText, setSearchText] = useState('');
 
   const onChangeShopSelected = ShopID => {
     const updatedSelection = selectedShopData?.includes(ShopID)
@@ -16,7 +18,16 @@ const ProductByShopFilter = ({selectedShopData, setSelectedShopData}) => {
 
   return (
     <View>
-      {allShopsLists?.data?.map((itm, index) => (
+      <SearchTextFiled
+        value={searchText}
+        handleTextSearch={value => setSearchText(value.toLowerCase())}
+      />
+      {(searchText !== ''
+        ? allShopsLists?.data?.filter(item =>
+            item?.shop_name.toLowerCase().includes(searchText),
+          )
+        : allShopsLists?.data
+      )?.map((itm, index) => (
         <View key={index}>
           <CheckBox
             title={capitalizeString(itm.shop_name)}
--- src/screens/FilterScreen/ShopFilterSubTab/ShopByLocation.js
+++ src/screens/FilterScreen/ShopFilterSubTab/ShopByLocation.js
@@ -1,13 +1,16 @@
 import {StyleSheet, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {CheckBox} from 'react-native-elements';
 import {capitalizeString} from '../../../common/CapitalizeString';
+import SearchTextFiled from '../../../common/SearchTextFiled';
 
 const ShopByLocation = ({
   areaLists,
   selectedLocationData,
   setSelectedLocationData,
 }) => {
+  const [searchText, setSearchText] = useState('');
+
   const handleMenCheckboxChange = itm => {
     const updatedSelection = selectedLocationData?.some(
       item => item.area === itm.area && item.pin === itm.pin,
@@ -20,7 +23,16 @@ const ShopByLocation = ({
 
   return (
     <View>
-      {areaLists?.map((itm, index) => (
+      <SearchTextFiled
+        value={searchText}
+        handleTextSearch={value => setSearchText(value.toLowerCase())}
+      />
+      {(searchText !== ''
+        ? areaLists?.filter(data =>
+            data?.area?.toLowerCase().includes(searchText),
+          )
+        : areaLists
+      )?.map((itm, index) => (
         <View key={index}>
           <CheckBox
             title={capitalizeString(itm?.area)}
