85f8611f6a8871b312a141f5d4d270a83351f2e8
gautam <gb949308@gmail.com>
2023-11-23 17:20:57 +0530

    account del done

--- src/DrawerNavigation/DrawerVendor.js
+++ src/DrawerNavigation/DrawerVendor.js
@@ -1,25 +1,32 @@
-import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 import VendorTab from '../TabNavigation/VendorTab';
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
   DrawerItemList,
 } from '@react-navigation/drawer';
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useDispatch} from 'react-redux';
-import {useToast} from 'native-base';
+import { useDispatch, useSelector } from 'react-redux';
+import { useToast } from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import VendorSideBarContent from '../components/VendorSideBarContent';
-import {userLogout} from '../redux/LoginUserProfileSlice/userSlice';
+import { userLogout } from '../redux/LoginUserProfileSlice/userSlice';
+import CustomButton from '../common/CustomButton';
+import { Divider } from 'react-native-paper';
+import { Modal } from 'react-native';
+import { deleteAccount } from '../graphql/mutations/accountDelete';
 
-const DrawerVendor = ({vendorShopDetails}) => {
+
+const DrawerVendor = ({ vendorShopDetails }) => {
   const navigation = useNavigation();
   const dispatch = useDispatch();
   const toast = useToast();
 
   const Drawer = createDrawerNavigator();
+  const [accDelModalVisible, setAccDelModalVisible] = useState(false);
+  const {userProfile, isAuthenticate} = useSelector(state => state?.user);
 
   const LogOut = async () => {
     AsyncStorage.clear();
@@ -37,15 +44,29 @@ const DrawerVendor = ({vendorShopDetails}) => {
 
   const CustomDrawerContent = props => {
     return (
-      <View style={{height: '100%', position: 'relative'}}>
+      <View style={{ height: '100%', position: 'relative', }}>
         <DrawerContentScrollView {...props}>
           <VendorSideBarContent vendorShopDetails={vendorShopDetails} />
           {/* <DrawerItemList {...props} /> */}
         </DrawerContentScrollView>
-        <TouchableOpacity onPress={() => LogOut()} style={styles.logoutMain}>
-          <Icon name="power-off" size={20} color="#151827" />
-          <Text style={styles.wishText}>logout</Text>
-        </TouchableOpacity>
+        <View style={styles.bottomMain}>
+          <Divider bold={true} />
+
+          <View style={{ width: '60%', marginLeft: 30, paddingVertical: 10 }}>
+            <CustomButton
+              name="Delete Account"
+              color="#FFFFFF"
+              backgroundColor="red"
+              borderColor="red"
+              onPress={() => setAccDelModalVisible(true)}
+            />
+          </View>
+          <TouchableOpacity onPress={() => LogOut()} style={styles.logoutMain}>
+            <Icon name="power-off" size={20} color="#151827" />
+            <Text style={styles.wishText}>logout</Text>
+          </TouchableOpacity>
+        </View>
+        <AccountDeleteModel userProfile={userProfile} accDelModalVisible={accDelModalVisible} setAccDelModalVisible={setAccDelModalVisible} />
       </View>
     );
   };
@@ -66,14 +87,19 @@ const DrawerVendor = ({vendorShopDetails}) => {
 export default DrawerVendor;
 
 const styles = StyleSheet.create({
+  bottomMain: {
+    bottom: 0,
+    position: 'absolute',
+    width: '100%',
+  },
   logoutMain: {
     flexDirection: 'row',
     alignItems: 'center',
     gap: 10,
     paddingHorizontal: 30,
     paddingVertical: 15,
-    bottom: 0,
-    position: 'absolute',
+    // bottom: 0,
+    // position: 'absolute',
     borderTopWidth: 1,
     width: '100%',
   },
@@ -82,4 +108,112 @@ const styles = StyleSheet.create({
     fontWeight: '400',
     fontSize: 18,
   },
+});
+
+
+const AccountDeleteModel = ({ userProfile , accDelModalVisible, setAccDelModalVisible }) => {
+  const toast = useToast();
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  const HandleDeleteAccount = async() => {
+    await deleteAccount({id: userProfile?.id}).then(
+      res => {
+        toast.show({
+          title: "Account Delete Sucessfully",
+          placement: 'top',
+          backgroundColor: 'green.600',
+          variant: 'solid',
+        });
+        setAccDelModalVisible(false)
+        AsyncStorage.clear();
+        dispatch(userLogout());
+        setTimeout(() => {
+          navigation.navigate('Splash');
+        }, 500);
+      },
+      error => {
+        toast.show({
+          title: error.message,
+          placement: 'top',
+          backgroundColor: 'red.600',
+          variant: 'solid',
+        });
+        setAccDelModalVisible(false)
+      },
+    );
+  }
+  return (
+    <View style={deltyles.centeredView}>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={accDelModalVisible}
+        onRequestClose={() => {
+          setAccDelModalVisible(!accDelModalVisible);
+        }}>
+        <View style={deltyles.centeredView}>
+          <View style={deltyles.modalView}>
+            <Text style={deltyles.titleText}>Delete Account</Text>
+            <Text style={deltyles.modalText} >All of your records will get removed permanently. Be aware that this action is irreversible</Text>
+            <View style={deltyles.bottomBtnMain}>
+              <View style={{ width: '40%' }}>
+                <CustomButton
+                  name="Delete"
+                  color="#FFFFFF"
+                  backgroundColor="red"
+                  borderColor="red"
+                  onPress={() => HandleDeleteAccount()}
+                />
+              </View>
+              <View style={{ width: '40%' }}>
+                <CustomButton
+                  name="Cancel"
+                  color="black"
+                  backgroundColor="white"
+                  borderColor="black"
+                  onPress={() => setAccDelModalVisible(false)}
+                />
+              </View>
+            </View>
+          </View>
+        </View>
+      </Modal>
+
+    </View>
+  );
+}
+
+const deltyles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    // alignItems: 'center',
+    width: "70%",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 15,
+    alignItems: 'center',
+    borderWidth: 1,
+  },
+  modalText: {
+    marginBottom: 15,
+    color: "black",
+    fontWeight: "400",
+    fontSize: 14
+  },
+  titleText: {
+    color: "black",
+    fontWeight: "600",
+    fontSize: 18,
+    paddingBottom: 5
+  },
+  bottomBtnMain : {
+    flexDirection : "row",
+    gap: 10
+  }
 });
